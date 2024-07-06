589d614981700b5bdac724876e888b9bea2fdfad
Shalayne Kemp <shaykemp89@gmail.com>
2024-07-06 12:51:23 -0700

    Part III - using arguments to query for specific objects

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,8 @@ const typeDefs = gql`
 type Query {
     "Get tracks arrayfor homepage grid"
     tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
 }
 
 "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +21,20 @@ type Track {
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
+}
+
+type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length:Int
 }
 
 "Author of a complete track"
