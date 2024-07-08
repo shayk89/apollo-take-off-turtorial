a488594767d7a72268e6a9c958edebbf1f715701
Shalayne Kemp <shaykemp89@gmail.com>
2024-07-08 11:41:09 -0700

    Part IV - Implemented a  Mutation
    Added mutation to schema, along with error handling, and used the useMutation hook to send a mutation from the client to the server

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,21 +2,41 @@ const resolvers = {
     Query: {
         // returns an array of Tracks that wil be used to populate
         // the homepage grid of our web client
-        tracksForHome: (_, __, {dataSources})=>{
+        tracksForHome: (_, __, { dataSources }) => {
 
             return dataSources.trackAPI.getTracksForHome();
         },
-        track:(_, {id}, {dataSources}) => {
+        track: (_, { id }, { dataSources }) => {
             return dataSources.trackAPI.getTrack(id);
 
         }
     },
-    Track:{
-        author:({authorId}, _, {dataSources}) => {
+    Mutation: {
+        incrementTrackViews: async (_, { id }, { dataSources }) => {
+            try{
+                const track = await dataSources.trackAPI.incrementTrackViews(id);
+                return {
+                    code: 200,
+                    success: true,
+                    message: `Successfuly incremented the number of views for track ${id}`,
+                    track,
+                }
+            }catch (err) {
+                return{
+                    code: err.extensions.response.status,
+                    success:false,
+                    message:err.extensions.response.body,
+                    track:null
+                }
+            }
+        }
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
             return dataSources.trackAPI.getAuthor(authorId);
         },
-        modules:({id}, _, {dataSources} ) => {
-                return dataSources.trackAPI.getTrackModules(id);
+        modules: ({ id }, _, { dataSources }) => {
+            return dataSources.trackAPI.getTrackModules(id);
         }
     },
 };
